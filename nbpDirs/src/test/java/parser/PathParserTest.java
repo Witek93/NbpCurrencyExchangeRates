06b11f32013d1b7c855b3fff22f23239206a3296
Witek93 <karol.witaszczyk@gmail.com>
2017-04-21 21:26:10 +0200

    Extend happy path PathParser unit test.

--- nbpDirs/src/test/java/parser/PathParserTest.java
+++ nbpDirs/src/test/java/parser/PathParserTest.java
@@ -1,6 +1,9 @@
 package parser;
 
+import model.ExchangeType;
 import org.junit.Test;
+
+import java.time.LocalDate;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
@@ -10,5 +13,8 @@ public class PathParserTest {
         PathParser pathParser = new PathParser("\uFEFFc001z150102");
 
         assertThat(pathParser.getId()).isEqualTo("001");
+        assertThat(pathParser.getType()).isEqualTo(ExchangeType.C);
+        assertThat(pathParser.getDate()).isEqualTo(LocalDate.of(2015, 1, 2));
+        assertThat(pathParser.getDirectoryPath()).isEqualTo("c001z150102");
     }
 }
