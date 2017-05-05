bc4c4c228c9432b2651c021370c75896ffbcc843
Witek93 <karol.witaszczyk@gmail.com>
2017-05-05 19:10:59 +0200

    Add negative test case for deserializing date with invalidly formatted date (as text).

--- Common/src/test/java/deserializers/LocalDateDeserializerTest.java
+++ Common/src/test/java/deserializers/LocalDateDeserializerTest.java
@@ -4,6 +4,7 @@ import com.fasterxml.jackson.core.JsonParser;
 import org.junit.Test;
 
 import java.time.LocalDate;
+import java.time.format.DateTimeParseException;
 
 import static org.assertj.core.api.Assertions.assertThat;
 import static org.mockito.Mockito.mock;
@@ -11,7 +12,7 @@ import static org.mockito.Mockito.when;
 
 public class LocalDateDeserializerTest {
     @Test
-    public void parsesLocalDate_forIsoFormat() throws Exception {
+    public void shouldPass_whenDeserializingLocalDate_forDateInIsoFormat() throws Exception {
         JsonParser jsonParser = mock(JsonParser.class);
         when(jsonParser.getText())
                 .thenReturn("2017-04-22");
@@ -21,4 +22,14 @@ public class LocalDateDeserializerTest {
         assertThat(deserializedDate)
                 .isEqualTo(LocalDate.of(2017, 4, 22));
     }
+
+    @Test(expected = DateTimeParseException.class)
+    public void shouldThrow_whenDeserializingLocalDate_forInvalidFormat() throws Exception {
+        JsonParser jsonParser = mock(JsonParser.class);
+        when(jsonParser.getText())
+                .thenReturn("99-12-31");
+
+        new LocalDateDeserializer().deserialize(jsonParser, null);
+    }
+
 }
